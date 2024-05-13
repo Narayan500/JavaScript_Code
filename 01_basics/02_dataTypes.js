3d7379749cede75dfc6794cc78c85c8983ddd20a
Narayan500 <147725461+Narayan500@users.noreply.github.com>
2024-05-13 07:19:54 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newwe version
+
+//alert (3+3) //we are using  node js,not browser
+
+let name = "ram"
+let age = 23
+let isLoggedIn = false
+let state;
+
+//datatype
+//number =>2 to power 53
+//bigint =>use big plate platfrom eg fb;
+//string => ""
+//boolean =>true/false
+//null =>standalone value
+//undefined =>value nahi diya hai
+//symbol =>unique
+
+//object
+
+console.log(typeof "ram")
+console.log(typeof null)//object
+console.log(typeof undefined)//output undefined
